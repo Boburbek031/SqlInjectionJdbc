68a1c1a6f85ef81d12487c43a254e3463ce07c76
Boburjon <raximovboburjon66@gmail.com>
2023-11-28 20:27:53 +0500

    authorizationWrittenWithStatement method is done

--- src/main/java/uz/ali/Main.java
+++ src/main/java/uz/ali/Main.java
@@ -1,12 +1,47 @@
 package uz.ali;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        // database uxladi hamma data lar ochib ketti
+//        Profile profile = authorizationWrittenWithStatement("ali';delete from profile;--", "alishman");
+        Profile profile = authorizationWrittenWithStatement("alish", "alishman");
+        System.out.println(profile);
 
-
-
-
+        // Boyagi xakkerskiy query bi yozsak ham ta'sir qila olmaydi
+//        Profile profile = authorizationWrittenWithPreparedtatement("ali';delete from profile;--", "alishman");
+  /*      Profile profile = authorizationWrittenWithPreparedtatement("alish", "alishman");
+        System.out.println(profile);*/
 
     }
+
+    // statement dan String larni qabul qilganda parametrlar bilan ishlaganda ishlatmaslik kerak
+    // xafi kotta, SQL injection ga olib keladi
+    public static Profile authorizationWrittenWithStatement(String login, String password) {
+        try (Connection connection = DatabaseUtil.getConnection();
+             Statement statement = connection.createStatement()) {
+            String selectQuery = "select * from profile where login = '%s' and password = '%s'";
+            selectQuery = String.format(selectQuery, login, password);
+//            System.out.println(selectQuery);
+
+            Profile profile = null;
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+
+            if (resultSet.next()) {
+                profile = new Profile(resultSet.getString("name"),
+                        resultSet.getString("surname"),
+                        resultSet.getString("login"),
+                        resultSet.getString("password"));
+            }
+            return profile;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
---
+++ src/main/java/uz/ali/Profile.java
@@ -0,0 +1,63 @@
+package uz.ali;
+
+public class Profile {
+
+
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+
+
+    public Profile() {
+    }
+
+    public Profile(String name, String surname, String login, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
